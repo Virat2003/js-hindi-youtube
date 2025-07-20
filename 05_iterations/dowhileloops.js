7c5f99d3bfc9d71d525a6309231bdbd8955c3d9a
Virat2003 <155090419+Virat2003@users.noreply.github.com>
2025-07-20 16:33:03 +0000

    higherorderloops

---
+++ 05_iterations/dowhileloops.js
@@ -0,0 +1,14 @@
+let myArray=["flash", "shaktiman", "superman"]
+let arr=0
+while (arr<myArray.length) {
+    console.log(myArray[arr]);
+    arr++
+    
+}
+
+let score=1
+do{
+    console.log(`the value is ${score}`);
+    score++
+    
+}while(score<=10)
---
+++ 05_iterations/higherorederarrayloops.js
@@ -0,0 +1,213 @@
+const countries=["maharashtra", "Goa", "karnataka", "up"]
+// for (const rajya of countries) {
+//     console.log(rajya);
+    
+// }
+for (let i = 0; i < countries.length; i++) {
+    // console.log(countries[i]);
+    
+    
+}
+
+// let greetings="hello world!"
+// for (const greet of greetings) {
+//     console.log(greet);
+    
+// }
+
+
+
+// let myObject={
+//     name:"vir",
+//     pin:416517
+// }
+
+// for (const mo of myObject) {
+//     console.log(m);
+    
+// }
+
+
+
+//map:-
+
+const map=new Map()
+// map.set('In',"India")
+// map.set('USA',"United states of America")
+// map.set('Fr',"France")
+
+// map.set("1",'a')
+// map.set("2",'b')
+// map.set("3",'c')
+// map.set("4",'d')
+
+
+
+// console.log(map);
+
+
+// for (const [key,value] of map) {
+//     // console.log(key,":-",value);
+    
+// }
+
+
+let myObject={
+    name:"virat",
+    price:"999",
+    js:"javascript",
+    cpp:"c++",
+    rb:"ruby"
+}
+
+// for (const key in myObject) {
+//         // console.log(`${key} full name is ${myObject[key]}`);
+        
+//     }
+
+
+
+for (const key in myObject) {
+console.log(`${key} and ${myObject.key}`);
+
+}
+
+
+
+    let myarray=["js","ruby","python","java"]
+// for (const key in myarray) {
+// //    console.log(myarray[key]);
+   
+// }
+for (const key of myarray) {
+    // console.log(key);
+    
+}
+
+
+
+let names=["sachin te", "virat kohli", " ms dhoni"]
+
+for (const key in names) {
+    // console.log(names[key]);
+    
+}
+
+map.set("1",'a')
+map.set("2",'b')
+map.set("3",'c')
+map.set("4",'d')
+
+for (const key in map) {
+    // console.log(key);
+    
+}
+
+
+
+const newArray=["js","ruby","python","java"]
+newArray.forEach(function (item) {
+    // console.log(item);
+    
+})
+
+
+newArray.forEach((item)=> {
+    // console.log(item);
+    
+})
+    
+
+
+myarray.forEach(function (item){
+    // console.log(item);
+    
+}
+    
+);
+
+
+
+myarray.forEach((item,index,arr)=>{
+    // console.log(item,index,arr);
+    
+})
+
+
+
+
+let abc={
+    name:"samba",
+    price:"666",
+    add:"redi"
+}
+
+for (const key in abc) {
+    // console.log(`${key} and ${abc[key]}`);
+    
+}
+
+
+let mya=["abcd","efgh","ijkl","mnop"]
+for (const key in mya) {
+    // console.log(mya[key]);
+    
+}
+
+
+
+
+for (const key of mya) {
+    // console.log(key);
+    
+}
+
+
+
+mya.forEach(function (item){
+    // console.log(item);
+    
+})
+
+mya.forEach((item)=>{
+    // console.log(item);
+    
+})
+
+function print(item){
+    // console.log(item);
+    
+}
+
+mya.forEach((item,index,arr)=>{
+    // console.log(item,index,arr);
+    
+})
+
+
+
+
+
+
+
+const myCoding=[{
+    languageName:"javascript",
+    languageFileName:"js",
+} ,
+
+{
+    languageName:"python",
+    languageFileName:"py",
+},
+
+   {
+    languageName:"java",
+    languageFileName:"java",
+   }
+
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageFileName);
+    
+})
