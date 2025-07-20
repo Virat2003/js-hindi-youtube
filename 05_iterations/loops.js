c132f9e5e8a8638452558bdb875234a5860e6d98
Virat2003 <155090419+Virat2003@users.noreply.github.com>
2025-07-20 07:06:47 +0000

    loops

---
+++ 05_iterations/loops.js
@@ -0,0 +1,42 @@
+for(let i=1;i<10;i++){
+    if (i==5) {
+        // console.log("5 is got");
+        continue
+    }
+    // console.log(i)
+}
+
+
+for (let i = 1; i < 10; i++) {
+    // console.log(`outer loop: ${i}`);
+    
+for (let j = 1; j < 10; j++) {
+    // console.log(`inner loop is ${j} and outer loop is ${i} `);
+    // console.log(i + '*' + j + ' = ' + i*j );
+    
+    
+    
+}    
+}
+
+
+let myArray=["virat", "vishal", "Anu" ]
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[2]);
+    
+    
+}
+
+
+// let mazoarray=[1,2,3,4,5,6]
+// console.log(mazoarray[1]);
+// for (let i = 1; i <=10;i++){
+//     for (let j = 1; j <=10; j++) {
+//         console.log(i + ' * ' + j + '=' + i*j);
+        
+        
+//     }
+// }
+
+
+
