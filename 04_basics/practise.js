60e4a0f343e101c211267af7925e327a0c791b4c
Virat2003 <155090419+Virat2003@users.noreply.github.com>
2025-07-20 05:58:23 +0000

    practise file

---
+++ 04_basics/practise.js
@@ -0,0 +1,38 @@
+// let myfun=(num1,num2)=>{
+//     return (num1+num2)
+// }
+
+// console.log(myfun(5,5))
+
+
+// let myfun=(num1,num2)=>(num1+num2)
+// console.log(myfun(5,5))
+
+
+
+// (function chai(){
+//     console.log("db connected");
+    
+// })()
+
+
+
+// ((name)=>{
+//     console.log("db connected");
+
+// })("virat")
+
+
+
+// const username=-0
+
+// if(username){
+//     console.log("allow");
+    
+// }else{
+//     console.log("dont allow");
+    
+// }
+
+
+
